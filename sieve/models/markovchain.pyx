4716a360b5bd663b2f5a1b9b0d963c492120656d
George Young <A-N-Other@users.noreply.github.com>
2017-11-28 21:11:31 +0000

    GY171128: fiddles

--- sieve/models/markovchain.pyx
+++ sieve/models/markovchain.pyx
@@ -2,6 +2,9 @@ from collections import defaultdict as dd
 from math import log
 
 
+__all__ = ['MarkovChain']
+
+
 class MarkovChain(object):
 
     def __init__(self, order=1):
@@ -10,6 +13,7 @@ class MarkovChain(object):
         self.mers = dd(int)
 
     def train(self, sequences):
+        ''' Build the mer dict based on input sequences '''
         for s in sequences:
             s = s.upper()
             for i in range(len(s) - self.order):
@@ -17,15 +21,15 @@ class MarkovChain(object):
                     self.mers[s[i:i+j]] += 1
 
     def _prob(self, obs):
-        ''' Probability of obs having had obs[:-1], or an estimate based on GC
-        content if obs has not been observed during training '''
+        ''' Probability of obs having had `obs`[:-1], or an estimate based on
+        GC content if `obs` has not been observed during training '''
         count_obs = self.mers.get(obs, None)
         if count_obs is None:
             return self._prob(obs[-1])
         return count_obs / self.mers[obs[:-1]]
 
     def prob(self, s):
-        ''' Return joint probability of observing string s '''
+        ''' Probability of observing `s` '''
         P = 1
         for i in range(1, self.order + 1):
             P *= self._prob(s[:i])
@@ -34,7 +38,7 @@ class MarkovChain(object):
         return P
 
     def logprob(self, s, base=2):
-        ''' Return joint probability of observing string s '''
+        ''' Log probability of observing `s` '''
         P = 0
         for i in range(1, self.order + 1):
             P += log(self._prob(s[:i]), base)
