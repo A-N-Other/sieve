0accf675de1750172aafab3fbcd9e3ccbddb61ee
A-N-Other <george_ry@hotmail.com>
2017-12-04 17:37:42 +0000

    GY171204: Added nw

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from distutils.core import setup, Extension
 from sys import maxsize
 
+from numpy import get_include
 from Cython.Build import cythonize
 
 
@@ -10,12 +11,18 @@ if not maxsize > 2**32:
     raise('Sieve requires compilation with a 64-bit Python3 / PyPy3 build')
 
 extensions = [
+    # structures
     Extension('sieve.structures.seqrecord', ['sieve/structures/seqrecord.pyx'],
         extra_compile_args=['-O3']),
+    # barray
     Extension('sieve.barray.barray', ['sieve/barray/barray.pyx'],
         extra_compile_args=['-O3']),
+    # utils
     Extension('sieve.utils.utils', ['sieve/utils/utils.pyx'],
         extra_compile_args=['-O3']),
+    Extension('sieve.utils.needle', ['sieve/utils/needle.pyx'],
+        extra_compile_args=['-O3'], include_dirs=[get_include()]),
+    # io
     Extension('sieve.io.i', ['sieve/io/i.pyx'],
         extra_compile_args=['-O3'], libraries=['z'])
     ]
